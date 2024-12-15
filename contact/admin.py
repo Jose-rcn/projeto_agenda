f79db4d5090e270bc095a9ecfea7e3203c0b7000
Jose-rcn <josercn55@gmail.com>
2024-12-15 18:54:07 -0300

    Add category model

--- contact/admin.py
+++ contact/admin.py
@@ -3,10 +3,16 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name','last_name','phone')
+    list_display = ('id','first_name','last_name','phone','category')
     ordering = '-id',
 #   list_filter = 'created_date'
     search_fields = 'id','first_name','last_name'
     list_per_page = 10
     list_display_links = 'id',
     list_editable = 'first_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id','name'
+    ordering = '-id',
+    list_display_links = 'id', 'name'
