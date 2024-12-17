36540f519c7e5374ba5265be645b94388ebe96c1
Jose-rcn <josercn55@gmail.com>
2024-12-17 20:25:52 -0300

    create search view

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
 from contact.models import Contact
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Q
 
 
 def index(request):
     Contacts = Contact.objects \
         .filter(show=True)\
+        .order_by('-id')[:10]
+
+    context = {
+        'contacts': Contacts,
+        'site_title': 'Contatos - Agenda'
+    }
+    return render(request, 'contact/index.html', context=context)
+
+
+def search(request,):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == "":
+        return redirect('contact:index')
+
+    print(search_value)
+    Contacts = Contact.objects \
+        .filter(Q(first_name__icontains=search_value) |
+                Q(last_name__icontains=search_value) |
+                Q(phone__icontains=search_value) |
+                Q(email__icontains=search_value) |
+                Q(id__icontains=search_value)
+                , show=True)\
         .order_by('-id')[:10]
 
     context = {
